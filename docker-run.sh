d960ce6068392e22be8a25a2f178bc51e8c97482
mliberty <mor12324@gmail.com>
2020-02-14 10:59:01 +0200

    add .

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sodu docker login --username mor12324 --password ml14678678
-sodu docker-compose up -d; sodu docker-compose push
-sodu docker logout
+sudo docker login --username mor12324 --password ml14678678
+sudo docker-compose up -d; sudo docker-compose push
+sudo docker logout
